ae6f3257d5a71edadf249b3384d6e9830c1eccc0
puppet <88923829@qq.com>
2019-05-13 18:09:21 +0800

    init

---
+++ security-demo/src/main/java/com/istimeless/securitydemo/config/BeetlSqlConfiguration.java
@@ -0,0 +1,34 @@
+package com.istimeless.securitydemo.config;
+
+import org.beetl.sql.core.ClasspathLoader;
+import org.beetl.sql.core.Interceptor;
+import org.beetl.sql.core.SQLManager;
+import org.beetl.sql.core.UnderlinedNameConversion;
+import org.beetl.sql.core.db.MySqlStyle;
+import org.beetl.sql.ext.DebugInterceptor;
+import org.beetl.sql.ext.spring4.BeetlSqlDataSource;
+import org.beetl.sql.ext.spring4.SqlManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author lijiayin
+ */
+@Configuration
+public class BeetlSqlConfiguration {
+    
+    @Bean
+    public SQLManager sqlManager(DataSource dataSource) throws Exception {
+        BeetlSqlDataSource beetlSqlDataSource = new BeetlSqlDataSource();
+        beetlSqlDataSource.setMasterSource(dataSource);
+        SqlManagerFactoryBean sqlManagerFactoryBean = new SqlManagerFactoryBean();
+        sqlManagerFactoryBean.setNc(new UnderlinedNameConversion());
+        sqlManagerFactoryBean.setCs(beetlSqlDataSource);
+        sqlManagerFactoryBean.setDbStyle(new MySqlStyle());
+        sqlManagerFactoryBean.setSqlLoader(new ClasspathLoader());
+        sqlManagerFactoryBean.setInterceptors(new Interceptor[]{new DebugInterceptor()});
+        return sqlManagerFactoryBean.getObject();
+    }
+}
---
+++ security-demo/src/main/java/com/istimeless/securitydemo/config/RedisConfiguration.java
@@ -0,0 +1,23 @@
+package com.istimeless.securitydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author lijiayin
+ */
+@Configuration
+public class RedisConfiguration {
+    
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory){
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        stringRedisTemplate.setDefaultSerializer(stringRedisSerializer);
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
+        return stringRedisTemplate;
+    }
+}
